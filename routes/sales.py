e05120d8a9c598fdf0a1739b25a09363d8d5ce1d
AlexDsJz <cduenasj1700@alumno.ipn.mx>
2023-05-14 21:32:07 -0600

    Add endpoints for create and update users (Superadmin)

--- routes/sales.py
+++ routes/sales.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate
-import seed.routes.users as SeedRoute
+import seed.routes.sales as SeedRoute
 from rest_framework.decorators import action
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -7,9 +7,6 @@ from rest_framework import status
 from rest_framework import permissions
 from django.shortcuts import get_object_or_404
 from seed.util.request_util import has_fields_or_400
-from app.models import User
-from app.serializers import UserSerializer
-from domain.utils import http_codes
 
 
 class SaleViewSet(SeedRoute.SaleViewSet):
--- routes/users.py
+++ routes/users.py
@@ -35,7 +35,6 @@ class UserViewSet(SeedRoute.UserViewSet):
         password = data["password"]
 
         user = authenticate(username=email, password=password)
-        print(user)
         company = user.company
 
         try:
@@ -115,15 +114,13 @@ class UserViewSet(SeedRoute.UserViewSet):
     @action(detail=False, methods=['POST'])
     def update_user_superadmin(self,request):
         data = request.data
-        print(data)
-        has_fields_or_400(data, 'user_id', 'city', 'cologn', 'cp', 'email', 'firstName', 'lastName', 'municipality', 'state', 'street', 'telephone', 'type', 'password', 'company')
-        update_info_superadmin(data['user_id'], data['city'], data['cologn'], data['cp'], data['email'], data['firstName'], data['password'],data['lastName'], data['municipality'], data['state'], data['street'], data['telephone'], data['type'], data['company'])
+        has_fields_or_400(data, 'user_id', 'city', 'cologn', 'company_id', 'cp', 'email', 'firstName', 'lastName', 'municipality', 'state', 'street', 'telephone', 'type', 'username', 'password')
+        update_info_superadmin(data['user_id'], data['city'], data['cologn'], data['company_id'], data['cp'], data['email'], data['firstName'], data['lastName'], data['municipality'], data['state'], data['street'], data['telephone'], data['type'], data['username'], data['password'])
         return Response(status=status.HTTP_200_OK)
     
     @action(detail=False, methods=['POST'])
     def update_user_normal(self,request):
         data = request.data
-        print(data)
         has_fields_or_400(data, 'user_id', 'city', 'cologn', 'cp', 'email', 'firstName', 'lastName', 'municipality', 'state', 'street', 'telephone', 'type', 'password', 'company')
         update_info_normal(data['user_id'], data['city'], data['cologn'], data['cp'], data['email'], data['firstName'], data['password'],data['lastName'], data['municipality'], data['state'], data['street'], data['telephone'], data['type'], data['company'])
         return Response(status=status.HTTP_200_OK)
@@ -131,7 +128,6 @@ class UserViewSet(SeedRoute.UserViewSet):
     @action(detail=False, methods=['POST'])
     def create_user_superadmin(self,request):
         data = request.data
-        print(data)
-        has_fields_or_400(data, 'city', 'cologn', 'cp', 'email', 'firstName', 'lastName', 'municipality', 'state', 'street', 'telephone', 'type', 'password', 'company')
-        create_info_superadmin( data['city'], data['cologn'], data['cp'], data['email'], data['firstName'], data['password'],data['lastName'], data['municipality'], data['state'], data['street'], data['telephone'], data['type'], data['company'])
+        has_fields_or_400(data, 'city', 'cologn', 'company_id', 'cp', 'email', 'firstName', 'lastName', 'municipality', 'password', 'state', 'street', 'telephone', 'type')
+        create_info_superadmin( data['city'], data['cologn'], data['company_id'], data['cp'], data['email'], data['firstName'], data['lastName'], data['municipality'], data['password'], data['state'], data['street'], data['telephone'], data['type'])
         return Response(status=status.HTTP_200_OK)
