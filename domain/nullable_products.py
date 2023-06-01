3a7d6bf0b5e3ca1e272d72406c70a44c650b1db0
VenenoParaLasHadas <rodrigo.olarteastudillo@protonmail.com>
2023-06-01 10:46:03 -0600

    fix null products

--- domain/nullable_products.py
+++ domain/nullable_products.py
@@ -2,8 +2,10 @@ from app.models import Sale, Product
 
 def change_value(saleId):
     sale = Sale.objects.get(id=saleId)
-    products = Product.objects.filter(sale_id = saleId)
+    print(sale)
+    products = Product.objects.filter(sale__id = saleId)
     for product in products:
-        product.sale_id = None
+        print(product)
+        product.sale = None
         product.save()
     sale.delete()
