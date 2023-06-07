d5552c59d891d040669a39161a6e55ae9d61cc61
VenenoParaLasHadas <rodrigo.olarteastudillo@protonmail.com>
2023-06-07 12:22:45 -0600

    fix create user company endpoint

--- domain/create_user_company.py
+++ domain/create_user_company.py
@@ -1,6 +1,7 @@
-from app.models import User
+from app.models import User, File
 
 def create_info_company( company_id, email, firstName, lastName, password, type):
+        photo = File.objects.get(pk=10001)
         user=User.objects.create(
                         token_verified=True,
                         username=email,
@@ -8,6 +9,8 @@ def create_info_company( company_id, email, firstName, lastName, password, type)
                         first_name=firstName,
                         last_name=lastName,
                         type=type,
+                        state = "NS",
+                        photo=photo,
                         company_id=company_id)
         user.set_password(password)
         user.save()
