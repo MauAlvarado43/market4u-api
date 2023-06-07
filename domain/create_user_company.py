8a741e7342919fc1b9b2918a64fd70ff95ec7842
VenenoParaLasHadas <rodrigo.olarteastudillo@protonmail.com>
2023-06-07 12:08:25 -0600

    fix bug create and update users company

--- domain/create_user_company.py
+++ domain/create_user_company.py
@@ -1,20 +1,12 @@
 from app.models import User
 
-def create_info_company( city, cologn, company_id, cp, email, firstName, lastName, municipality, password, state, street, telephone, type):
-    if(type == "SELLER" or type == "ADMIN"):
+def create_info_company( company_id, email, firstName, lastName, password, type):
         user=User.objects.create(
-                        city=city,
-                        cologn=cologn,
                         token_verified=True,
-                        cp=cp,
                         username=email,
                         email=email,
                         first_name=firstName,
                         last_name=lastName,
-                        municipality=municipality,
-                        state=state,
-                        street=street,
-                        telephone=telephone,
                         type=type,
                         company_id=company_id)
         user.set_password(password)
--- domain/update_user_normal.py
+++ domain/update_user_normal.py
@@ -1,17 +1,10 @@
 from app.models import User, Company
 
-def update_info_normal(user_id, city, cologn, cp, email, firstName, password, lastName, municipality, state, street, telephone, type,company):
+def update_info_normal(user_id, email, firstName, password, lastName, type,company):
     user = User.objects.get(id=user_id)
-    user.city = city
-    user.cologn = cologn
-    user.cp = cp
     user.email = email
     user.first_name = firstName
     user.last_name = lastName
-    user.municipality = municipality
-    user.state = state
-    user.street = street
-    user.telephone = telephone
     user.type = type
     user.company = Company.objects.get(id=company)
     if(password != ''):
